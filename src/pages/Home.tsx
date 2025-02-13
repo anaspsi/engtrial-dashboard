9950fb20d04acf6df1acca6ce85f2e6ee219e01e
Ana Suryana di PSI <ana.suryana@smt.co.id>
2025-02-13 13:41:05 +0700

    feat : main detail

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useRef, useState } from 'react'
 import '../home.css'
-import { Badge, Container, ProgressBar } from "react-bootstrap"
+import { Badge, Card, Container, ProgressBar } from "react-bootstrap"
 import axios from "axios"
 import { useSearchParams } from 'react-router'
 import numeral from 'numeral'
@@ -14,7 +14,9 @@ export default function Home({ userInfo }: { userInfo: any }) {
     })
 
     const [isSearching, setIsSearching] = useState(false)
+    const [isSearchingD1, setIsSearchingD1] = useState(false)
     const [theMachine, setTheMachine] = useState('')
+    const [lineCode, setLineCode] = useState('')
     const [searchParam] = useSearchParams();
 
     const refInputDate1 = useRef(null)
@@ -26,10 +28,15 @@ export default function Home({ userInfo }: { userInfo: any }) {
     }
 
     const [rowData, setRowData] = useState({ data: [] })
+    const [rowDataDetail1, setRowDataDetail1] = useState({ data: [] })
 
     function handleClickSearch() {
+        setLineCode('')
         const params = new URLSearchParams(formData).toString()
-
+        setIsSearching(true)
+        setRowDataDetail1({
+            data: []
+        })
         axios.get(import.meta.env.VITE_APP_ENDPOINT + '/engtrial/report1?' + params + '&machineBrand=' + theMachine)
             .then((response) => {
                 const datanya = response.data.data
@@ -40,7 +47,6 @@ export default function Home({ userInfo }: { userInfo: any }) {
 
             }).catch(() => {
                 setIsSearching(false)
-
             })
     }
 
@@ -48,13 +54,16 @@ export default function Home({ userInfo }: { userInfo: any }) {
     useEffect(() => {
         // atur freeze first row
         let aTable = document.getElementById('mainTable')
+        let aTable1 = document.getElementById('detailTable1')
         let aStack1 = document.getElementById('stack1')
-        let aStack8 = document.getElementById('stack8')
-        if (aTable && aStack1 && aStack8) {
+        if (aTable && aStack1 && aTable1) {
             aTable.style.cssText = `height: ${window.innerHeight
                 - aStack1.offsetHeight
-                - aStack8.offsetHeight
-                - 130
+                - 175
+                }px`
+            aTable1.style.cssText = `height: ${window.innerHeight
+                - aStack1.offsetHeight
+                - 175
                 }px`
         }
 
@@ -70,6 +79,26 @@ export default function Home({ userInfo }: { userInfo: any }) {
         }
     }, [])
 
+    function handleClickMainView(data: any) {
+        setLineCode(data.line)
+        const params = new URLSearchParams(formData).toString()
+        setIsSearchingD1(true)
+        axios.get(import.meta.env.VITE_APP_ENDPOINT + '/engtrial/reportd1?' + params + '&machineBrand=' + theMachine
+            + '&lineCode=' + data.line
+            + '&machineCode=' + data.machineCode)
+            .then((response) => {
+                setIsSearchingD1(false)
+                const datanya = response.data.data
+                setRowDataDetail1({
+                    data: datanya
+                })
+
+
+            }).catch(() => {
+                setIsSearchingD1(false)
+            })
+    }
+
     return (
         <Container fluid>
             <form>
@@ -81,7 +110,7 @@ export default function Home({ userInfo }: { userInfo: any }) {
                         </div>
                     </div>
                     <div className="col-md-6">
-                        <div className="input-group input-group-sm mb-1">
+                        <div className="input-group input-group-sm mb-3">
                             <span className="input-group-text" > Period from</span>
                             <input type="date" className="form-control" name="dateFrom" onChange={handleChange} ref={refInputDate1} />
                             <span className="input-group-text" >To</span>
@@ -90,52 +119,98 @@ export default function Home({ userInfo }: { userInfo: any }) {
                         </div>
                     </div>
                 </div>
-
-                <div className="row" id="stack8">
-                    <div className="col-md-4 mb-3">
-                        <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-3 text-center">
-
-                    </div>
-                    <div className="col-md-4 mb-3 text-end">
-                        {isSearching ? '' : <Badge bg="info">{rowData.data.length > 0 ? rowData.data.length + ' rows found' : ''}</Badge>}
-                    </div>
-                </div>
             </form>
 
             <div className="row">
-                <div className="col-md-12 mb-1">
-                    <div className="table-responsive" id="mainTable">
-                        <table className="table align-middle table-sm table-bordered table-hover" >
-                            <thead className="text-center table-light">
-                                <tr className="first">
-                                    <th className="align-middle">Date</th>
-                                    <th className="align-middle">Line</th>
-                                    <th className="align-middle">Machine Number</th>
-                                    <th className="align-middle">Check</th>
-                                    <th className="align-middle">Pass</th>
-                                    <th className="align-middle">Pass Rate</th>
-                                </tr>
-                            </thead>
-                            <tbody className='text-center'>
-                                {
-                                    isSearching ? <tr><td colSpan={16}>Please wait</td></tr> : rowData.data.map((item: any, index) => {
-                                        return <tr key={index} className="font-monospace">
-                                            <td>{item.txttgl}</td>
-                                            <td>{item.txtline}</td>
-                                            <td>{item.txtict}</td>
-                                            <td>{numeral(item.txtcheck).format(',')}</td>
-                                            <td>{numeral(item.txtpass).format(',')}</td>
-                                            <td><ProgressBar now={item.txtpercen} label={`${item.txtpercen}%`} /></td>
-                                        </tr>
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                    </div>
+                <div className="col-md-6 mb-1">
+                    <Card>
+                        <Card.Header>
+                            <div className="d-flex">
+                                <div className="me-auto p-2">Main View</div>
+                                <div className="p-2"> {isSearching ? 'Please wait' : <Badge bg="info">{rowData.data.length > 0 ? rowData.data.length + ' rows found' : ''}</Badge>}</div>
+                            </div>
+                        </Card.Header>
+                        <Card.Body>
+                            <Container fluid>
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <div className="table-responsive" id="mainTable">
+                                            <table className="table align-middle table-sm table-bordered table-hover" >
+                                                <thead className="text-center table-light">
+                                                    <tr className="first">
+                                                        <th className="align-middle">Line</th>
+                                                        <th className="align-middle">Machine Number</th>
+                                                        <th className="align-middle">Check</th>
+                                                        <th className="align-middle">Pass</th>
+                                                        <th className="align-middle">Pass Rate</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody className='text-center'>
+                                                    {
+                                                        isSearching ? <tr><td colSpan={16}>Please wait</td></tr> : rowData.data.map((item: any, index) => {
+                                                            return <tr key={index} className="font-monospace">
+                                                                <td onClick={() => handleClickMainView({ line: item.txtline, machineCode: item.txtict })} title='See detail' style={{ cursor: 'pointer' }}>{item.txtline}</td>
+                                                                <td>{item.txtict}</td>
+                                                                <td>{numeral(item.txtcheck).format(',')}</td>
+                                                                <td>{numeral(item.txtpass).format(',')}</td>
+                                                                <td><ProgressBar now={item.txtpercen} label={`${item.txtpercen}%`} /></td>
+                                                            </tr>
+                                                        })
+                                                    }
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Container>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-6 mb-1">
+                    <Card>
+                        <Card.Header>
+                            <div className="d-flex">
+                                <div className="me-auto p-2">Detail View 1 <Badge bg='info'>{lineCode}</Badge> </div>
+                                <div className="p-2">.</div>
+                            </div>
+                        </Card.Header>
+                        <Card.Body>
+                            <Container fluid>
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        <div className="table-responsive" id="detailTable1">
+                                            <table className="table align-middle table-sm table-bordered table-hover" >
+                                                <thead className="text-center table-light">
+                                                    <tr className="first">
+                                                        <th className="align-middle">Date</th>
+                                                        <th className="align-middle">Line</th>
+                                                        <th className="align-middle">Machine Number</th>
+                                                        <th className="align-middle">Check</th>
+                                                        <th className="align-middle">Pass</th>
+                                                        <th className="align-middle">Pass Rate</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody className='text-center'>
+                                                    {
+                                                        isSearchingD1 ? <tr><td colSpan={16}>Please wait</td></tr> : rowDataDetail1.data.map((item: any, index) => {
+                                                            return <tr key={index} className="font-monospace">
+                                                                <td>{item.txttgl}</td>
+                                                                <td>{item.txtline}</td>
+                                                                <td>{item.txtict}</td>
+                                                                <td>{numeral(item.txtcheck).format(',')}</td>
+                                                                <td>{numeral(item.txtpass).format(',')}</td>
+                                                                <td><ProgressBar now={item.txtpercen} label={`${item.txtpercen}%`} /></td>
+                                                            </tr>
+                                                        })
+                                                    }
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Container>
+                        </Card.Body>
+                    </Card>
                 </div>
             </div>
         </Container>
